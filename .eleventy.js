d984a907166ea84fed77d50da4a2994b71f7e556
ksuzue <suzuekar@msu.edu>
2025-12-24 10:59:01 -0500

    first commit! testing out eleventy

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const htmlmin = require("html-minifier-terser");
+
+module.exports = function (eleventyConfig) {
+
+  // This will stop the default behaviour of foo.html being turned into foo/index.html
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  // This will make Eleventy not use your .gitignore file to ignore files
+  eleventyConfig.setUseGitIgnore(false);
+
+  // This will copy this folder to the output without modifying it at all
+  eleventyConfig.addPassthroughCopy("content/assets");
+
+  // This defines which files will be copied
+  eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
+
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  return {
+    dir: {
+      input: "src", // templates and pages
+      output: "public", // generated, finished html files uploaded to web
+    },
+  };
+};
